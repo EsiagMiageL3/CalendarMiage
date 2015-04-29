663c3a9b20cd2fea225ca848c60ccba2d8071361
CyrielleM <min.cyrielle@yahoo.com>
2015-04-29 19:07:27 +0200

    f

--- src/modele/Seance.java
+++ src/modele/Seance.java
@@ -1,30 +1,39 @@
 package modele;
 
+import java.io.Serializable;
+
 import exception.RechercheSeanceException;
-public class Seance {
-	protected String nom ;
-	protected String abreviation;
-	protected String couleur;
-	protected int nbreSeance; 
-	protected int rangSeance;
+import modele.Module;
+
+public class Seance extends Module implements Serializable{
+	public int rang_seances;
 	
-	public Seance(String nom, String abreviation, String couleur, int n, int m){
-					this.nom = nom;
-					this.abreviation = abreviation;
-					this.couleur = couleur;
-					this.nbreSeance = n;
-					this.rangSeance = m; 
+	public Seance (String nom_m, String abreviation, String couleur,int nb_seances,int nb_heures){
+		super(nom_m, abreviation, couleur, nb_seances, nb_heures);	
+	}
+
+	
+	public Seance (String nom_m, String abreviation, String couleur,int nb_seances,int nb_heures, int n){
+					super(nom_m, abreviation, couleur, nb_seances, nb_heures);	
+					this.rang_seances = n;
 	}
 	
-	
-	public void AjoutSeance( int n, int m) throws RechercheSeanceException {
-			if (n > m) throw new RechercheSeanceException();
+	public void AjoutSeance(String nom_module, int n) throws RechercheSeanceException {
+		if super.nom_m.equals(nom_module);
+		if (n > m.getNb_Seance()) throw new RechercheSeanceException();
 			else{
-				this.nbreSeance = n;
+				this.nb_heures = nb_seances;
 				System.out.println("Seance crée");
+				
 				}
 	}
 	
-	
-	
+	//Constructeur 
+	public int getrang_seances() {
+		return rang_seances;
+	}
+	public void setrang_seances(int n) {
+		this.rang_seances = n;
+	}
+
 }
